d458e4763c4c4c718c4d4fb8001d0085270537dc
ROBOT\pavel <pavelpetrochenko2001@gmail.com>
2022-12-01 23:30:10 +0300

    reorgonized components and added style.css

---
+++ src/componets/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Wheel from './Wheel'
+
+export default function App () {
+    return (
+        <div class="screen">
+            <Wheel />
+        </div>
+    );
+}
---
+++ src/componets/Wheel.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Wheel () {
+    return <div id="wheel-bg"></div>
+}
