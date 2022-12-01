bb1d4296daf444953363fb49c0cdfbab970a544e
HebiSunrise <vladguzikov13@gmail.com>
2022-12-01 19:47:08 +0300

    start of the wheel

---
+++ src/Wheel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+const Wheel = () => {
+    var divStyle = {
+        margin: '40px',
+        border: '5px solid pink',
+    };
+
+    return <div style={divStyle}> ds</div> 
+}
+
+export default Wheel
